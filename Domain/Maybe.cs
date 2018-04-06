ab718b2fd82ed9d342cb6459a7b1b6c973a42256
David Osborne <davidmosborne@yahoo.com>
2018-04-06 14:48:33 +0100

    Maybe;

---
+++ Domain/Maybe.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// http://blog.ploeh.dk/2018/03/26/the-maybe-functor/
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class Maybe<T>
+    {
+        public Maybe()
+        {
+            HasItem = false;
+        }
+
+        public Maybe(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            HasItem = true;
+            Item = item;
+        }
+
+        internal bool HasItem { get; }
+        internal T Item { get; }
+
+        public Maybe<TResult> Select<TResult>(Func<T, TResult> selector)
+        {
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+
+            return HasItem ? new Maybe<TResult>(selector(Item)) : new Maybe<TResult>();
+        }
+
+        public T GetValueOrFallback(T fallbackValue)
+        {
+            if (fallbackValue == null)
+            {
+                throw new ArgumentNullException(nameof(fallbackValue));
+            }
+
+            return HasItem ? Item : fallbackValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Maybe<T> other))
+            {
+                return false;
+            }
+                
+            return Equals(Item, other.Item);
+        }
+
+        public override int GetHashCode()
+        {
+            return HasItem ? Item.GetHashCode() : 0;
+        }
+    }
+}
---
+++ Domain/Tests/MaybeTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Domain.Tests
+{
+    public class MaybeTests
+    {
+        [Fact]
+        public void Maybe()
+        {
+            var m = new Maybe<int>(42);
+
+            var n = m.Select(s => s.ToString());
+
+            var x = n.GetValueOrFallback("");
+        }
+    }
+}

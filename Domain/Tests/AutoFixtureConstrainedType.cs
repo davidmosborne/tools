ba4f3d631f2bc6c346526193ed43b9f53965feba
David Osborne <davidmosborne@yahoo.com>
2018-05-18 12:04:38 +0100

    AutoFixture Ideas;

---
+++ Domain/Tests/AutoFixtureConstrainedType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class AutoFixtureConstrainedType
+    {
+        [Fact]
+        public void CreateConstrainedType()
+        {
+            var fixture = new Fixture();
+
+            fixture.Register<int, DateTime>(i => DateTime.UtcNow.AddDays(i).Date);
+
+            var date = fixture.Create<DateTime>();
+        }
+    }
+}
---
+++ Domain/Tests/AutoFixtureCustomization.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class AutoFixtureCustomization
+    {
+        [Fact]
+        public void TestWithCustomizations()
+        {
+            var fixture = new Fixture().Customize(new MyCustomizations());
+
+            fixture.Create<string>().Should().Contain("me!");
+        }
+
+        private class MyCustomizations : CompositeCustomization
+        {
+            public MyCustomizations()
+                : base(
+                    new AutoMoqCustomization(),
+                    new MyCustomization())
+            {
+            }
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// ...this is the perfect place to encapsulate common Customizations...
+        /// http://blog.ploeh.dk/2011/03/18/EncapsulatingAutoFixtureCustomizations/
+        /// </summary>
+        private class MyCustomization : ICustomization
+        {
+            public void Customize(IFixture fixture)
+            {
+                fixture.Inject("You're only going to get me!");
+            }
+        }
+    }
+}
